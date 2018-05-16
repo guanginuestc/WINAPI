892bc13fdfd9f4fd6a4da75b9494a109c51e6a7b
guanginuestc <h_n_sjg@163.com>
2018-05-16 21:43:00 +0800

    修复bug，调试结束后不影响原程序执行

--- usefunofexe/usefuncofexe.cpp
+++ usefunofexe/usefuncofexe.cpp
@@ -41,12 +41,49 @@ vector<DWORD> GetProcessIDByName(LPCTSTR szProcessName)
 	CloseHandle(hSnapshot);
 	return dwPID;//返回容器
 }
+bool RemoveBP(vector<DWORD> processid, BYTE &torecovery) {
+	return TRUE;
+}
+bool SetByte(vector<DWORD> processid, BYTE &org,BYTE &BP,DWORD addr) {
+	/*
+	修改对应进程的某位字符，将addr地址的字节拷贝到org中，并修改为BP中的字节
+	*/
 
-void DebugLoop() {
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processid[0]);
+	if (hProcess == NULL) {
+		cout << "open failed!" << endl;
+		return FALSE;
+	}
+
+	
+	DWORD numofwriten;
+
+	if (!ReadProcessMemory(hProcess, (LPVOID)addr, (LPVOID)&org, sizeof(org), &numofwriten)) {
+		cout << "read failed!" << endl;
+		CloseHandle(hProcess);
+		return FALSE;
+
+	}
+	if (!WriteProcessMemory(hProcess, (LPVOID)addr, (LPCVOID)&BP, 1, &numofwriten)) {//将断点写入到程序中。
+		cout << "write memory failed!" << endl;
+		CloseHandle(hProcess);
+		return FALSE;
+	}
+	CloseHandle(hProcess);
+	return TRUE;
+}
+void DebugLoop(vector<DWORD> processid) {
 	DEBUG_EVENT de;
 	DWORD dwContinueStatus;
 	DWORD arg[] = { 0x01,0x05 };
+	BYTE torecovery = 0x00, BP = 0xCC;
+	DWORD addr = 0x004010AF;
+	if (!SetByte(processid, torecovery, BP, addr)) {
+		cout << "设置断点失败" << endl;
+		return;
+	}
 	while (WaitForDebugEvent(&de, INFINITE)) {//breakpoint
+		
 		dwContinueStatus = DBG_CONTINUE;
 		if (EXCEPTION_BREAKPOINT == de.u.Exception.ExceptionRecord.ExceptionCode) {
 			CONTEXT ctx;
@@ -56,49 +93,54 @@ void DebugLoop() {
 			if (ctx.Eip == 0x004010B0) {
 				HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, de.dwProcessId);
 				DWORD numofwrite;
-				cout << arg[0] << "+" << arg[1] << ":" << ctx.Eax << "\tesp"<<ctx.Esp<<endl;
-				ctx.Esp -= 2*sizeof(DWORD);
+				cout << arg[0] << "+" << arg[1] << ":" << ctx.Eax << "\tesp" << ctx.Esp << endl;
+				printf("input new number:");
+				scanf("%d %d", &arg[0], &arg[1]);
+				if (!(arg[0]&& arg[1])) {
+					ctx.Eip--;
+					SetThreadContext(hThread, &ctx);
+					SetByte(processid, BP, torecovery, addr);
+					CloseHandle(hThread);
+					CloseHandle(hProcess);
+					ContinueDebugEvent(de.dwProcessId, de.dwThreadId, dwContinueStatus);
+					return;
+				}
+				ctx.Esp -= 2 * sizeof(DWORD);
 				WriteProcessMemory(hProcess, (LPVOID)ctx.Esp, arg, sizeof(DWORD)*sizeof(arg), &numofwrite);
 				ctx.Eip = 0x004010A7;
 				SetThreadContext(hThread, &ctx);
+				
+				CloseHandle(hThread);
 				CloseHandle(hProcess);
+				
 			}
-			CloseHandle(hThread);
+			else {
+				CloseHandle(hThread);
+			}
 
 		}
 		ContinueDebugEvent(de.dwProcessId, de.dwThreadId, dwContinueStatus);
 	}
 }
 int main() {
-	WCHAR processname[] = TEXT("testfoo.exe");
+	WCHAR processname[] = TEXT("testfoo.exe");//调用的进程名称
 	vector<DWORD> processid = GetProcessIDByName(processname);
 	if (processid.size() == 0) {
 		wprintf(L"process %s not found\n", processname);
-		return 0;
+		return FALSE;
 	}
 	
-	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processid[0]);
-	if (hProcess == NULL) {
-		cout << "open failed!" << endl;
-		return 0;
-	}
-
-	DWORD sp []= { 0xcc };
-	DWORD esp = 0x004010AF;
-	DWORD numofwriten;
-
-	if (!WriteProcessMemory(hProcess, (LPVOID)esp, (LPCVOID)sp, sizeof(DWORD) * sizeof(sp), &numofwriten)) {
-		cout << "write memory failed!" << endl;
-	}
-	CloseHandle(hProcess);
-	if (!DebugActiveProcess(processid[0])) {
+	
+	
+	if (!DebugActiveProcess(processid[0])) {//调试打开进程
 		cout << "debug failed!" << endl;
 	}
 	else {
 		wprintf(L"begin debug %s\n", processname);
 	}
 
-	DebugLoop();
+	DebugLoop(processid);
+	
 	if (!DebugActiveProcessStop(processid[0])) {
 		cout << "stop debug failed!" << endl;
 	}
