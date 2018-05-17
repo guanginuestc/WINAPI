b3d2ab9d9b5dba570b36c111bce57f8a2bce4252
guanginuestc <h_n_sjg@163.com>
2018-05-17 19:28:08 +0800

    程序模块化，修复bug

--- usefunofexe/usefuncofexe.cpp
+++ usefunofexe/usefuncofexe.cpp
@@ -72,42 +72,48 @@ bool SetByte(vector<DWORD> processid, BYTE &org,BYTE &BP,DWORD addr) {
 	CloseHandle(hProcess);
 	return TRUE;
 }
-void DebugLoop(vector<DWORD> processid) {
+void DebugLoop(vector<DWORD> processid,DWORD BP_position,DWORD LoopStart) {
+	/*
+	循环调用函数主体
+	processid：对应关心进程的id
+	BP_position：函数执行结束平衡堆栈后的第一条语句地址
+	LoopStart：函数执行的地址
+	*/
 	DEBUG_EVENT de;
 	DWORD dwContinueStatus;
-	DWORD arg[] = { 0x01,0x05 };
+	DWORD arg[] = { 0x01,0x05 };//参数列表
 	BYTE torecovery = 0x00, BP = 0xCC;
-	DWORD addr = 0x004010AF;
-	if (!SetByte(processid, torecovery, BP, addr)) {
+	DWORD addr = BP_position;
+	if (!SetByte(processid, torecovery, BP, addr)) {//设置断点
 		cout << "设置断点失败" << endl;
 		return;
 	}
 	while (WaitForDebugEvent(&de, INFINITE)) {//breakpoint
 		
 		dwContinueStatus = DBG_CONTINUE;
-		if (EXCEPTION_BREAKPOINT == de.u.Exception.ExceptionRecord.ExceptionCode) {
+		if (EXCEPTION_BREAKPOINT == de.u.Exception.ExceptionRecord.ExceptionCode) {//检测程序当前是否为Int3异常
 			CONTEXT ctx;
 			ctx.ContextFlags = CONTEXT_ALL;
 			HANDLE hThread = OpenThread(THREAD_ALL_ACCESS, NULL, de.dwThreadId);
 			GetThreadContext(hThread, &ctx);
-			if (ctx.Eip == 0x004010B0) {
+			if (ctx.Eip == BP_position+1) {
 				HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, de.dwProcessId);
 				DWORD numofwrite;
-				cout << arg[0] << "+" << arg[1] << ":" << ctx.Eax << "\tesp" << ctx.Esp << endl;
+				cout << arg[0] << "+" << arg[1] << ":"<< ctx.Eax <<std::oct<< "\tesp" << std::hex << ctx.Esp << endl;
 				printf("input new number:");
 				scanf("%d %d", &arg[0], &arg[1]);
-				if (!(arg[0]&& arg[1])) {
-					ctx.Eip--;
-					SetThreadContext(hThread, &ctx);
-					SetByte(processid, BP, torecovery, addr);
+				if (!(arg[0]&& arg[1])) {//如果接收到参数均为0，则调试器进行退出和复位程序
+					ctx.Eip--;//将eip指向正确的位置
+					SetThreadContext(hThread, &ctx);//修改线程上下文，让程序恢复正常
+					SetByte(processid, BP, torecovery, addr);//删除下的断点
 					CloseHandle(hThread);
 					CloseHandle(hProcess);
 					ContinueDebugEvent(de.dwProcessId, de.dwThreadId, dwContinueStatus);
 					return;
 				}
-				ctx.Esp -= 2 * sizeof(DWORD);
-				WriteProcessMemory(hProcess, (LPVOID)ctx.Esp, arg, sizeof(DWORD)*sizeof(arg), &numofwrite);
-				ctx.Eip = 0x004010A7;
+				ctx.Esp -= sizeof(arg);
+				WriteProcessMemory(hProcess, (LPVOID)ctx.Esp, arg, sizeof(DWORD)*sizeof(arg), &numofwrite);//将参数写入到进行堆栈中
+				ctx.Eip = LoopStart;
 				SetThreadContext(hThread, &ctx);
 				
 				CloseHandle(hThread);
@@ -124,6 +130,8 @@ void DebugLoop(vector<DWORD> processid) {
 }
 int main() {
 	WCHAR processname[] = TEXT("testfoo.exe");//调用的进程名称
+	DWORD BP_position = 0x004010AF;
+	DWORD LoopStart = 0x004010A7;
 	vector<DWORD> processid = GetProcessIDByName(processname);
 	if (processid.size() == 0) {
 		wprintf(L"process %s not found\n", processname);
@@ -139,9 +147,9 @@ int main() {
 		wprintf(L"begin debug %s\n", processname);
 	}
 
-	DebugLoop(processid);
+	DebugLoop(processid, BP_position,LoopStart);//循环调用对应的函数
 	
-	if (!DebugActiveProcessStop(processid[0])) {
+	if (!DebugActiveProcessStop(processid[0])) {//停止调试进程
 		cout << "stop debug failed!" << endl;
 	}
 	else {
